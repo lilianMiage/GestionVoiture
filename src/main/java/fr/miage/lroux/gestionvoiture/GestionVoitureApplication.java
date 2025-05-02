6b3ae196b45cfddecfa24dca8aa8ff644e96c96c
Lilianroux <lilianroux02@gmail.com>
2025-05-02 12:24:24 +0200

    first commit

---
+++ src/main/java/fr/miage/lroux/gestionvoiture/GestionVoitureApplication.java
@@ -0,0 +1,13 @@
+package fr.miage.lroux.gestionvoiture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionVoitureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionVoitureApplication.class, args);
+    }
+
+}
